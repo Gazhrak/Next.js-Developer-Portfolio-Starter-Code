d886f67cc291e0afeb923c06d2b8d107ad943dae
Maren Aranburo <maren.aranburo.etxeondo@gmail.com>
2023-05-02 18:58:44 +0200

    primeros pasos para poner el modo nocturno

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
-import { LinkedInIcon,GithubIcon } from './Icons'
+import { LinkedInIcon,GithubIcon, SunIcon, MoonIcon } from './Icons'
 import {motion} from 'framer-motion'
+import useThemeSwitcher from './hooks/useThemeSwitcher'
 
 const CustomLink = ({href,title, className=""}) => {
   const router = useRouter();
@@ -19,6 +20,8 @@ const CustomLink = ({href,title, className=""}) => {
 }
 
 const NavBar = () => {
+
+  const [mode, setMode] = useThemeSwitcher();
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
@@ -40,6 +43,15 @@ const NavBar = () => {
         >
           <LinkedInIcon />
         </motion.a>
+
+        <button 
+        onClick={()=> setMode(mode === 'light' ? 'dark' : "light")}>
+          {
+            mode === 'dark' ? 
+            <SunIcon className={"fill-dark"}/> :
+            <MoonIcon className={"fill-dark"}/>
+          }
+        </button>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo/></div>
